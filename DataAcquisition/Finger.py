7fb114b165f1aa656c10211508beff167f86b9c2
Zixin Zhang <1186193932@qq.com>
2020-08-24 16:54:37 +0800

    motion control complete

--- DataAcquisition/Finger.py
+++ DataAcquisition/Finger.py
@@ -1,13 +1,6 @@
 class Finger:
 
-    def __init__(self, a, b, h, theta, alpha, beta):
-        self.a = a
-        self.b = b
-
-        self.h_1 = h[0]
-        self.h_2 = h[1]
-        self.h_3 = h[2]
-
+    def __init__(self, theta, alpha, beta):
         self.theta_1 = theta[0]
         self.theta_2 = theta[1]
         self.alpha = alpha
